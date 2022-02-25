50042729936250a49e2c911d22e94ed548039830
gharbiamine <gharbiamine5674@gmail.com>
2022-02-25 12:48:56 +0100

    initial commit

---
+++ src/pages/home-page/Homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./homepage.module.css";
+
+const Homepage = () => {
+  return (
+    <div className={styles.global}>
+      <h1>
+        Ea exercitation elit et non sit do non ad nisi do. Dolor id mollit duis
+        sunt proident voluptate anim esse quis dolore. Excepteur reprehenderit
+        aute nostrud eu id sint ullamco occaecat est cillum proident duis
+        nostrud dolore. Aute laboris id nostrud enim dolor cillum eiusmod ut id
+        occaecat qui tempor amet.
+      </h1>
+    </div>
+  );
+};
+
+export default Homepage;
