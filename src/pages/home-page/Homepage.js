696ec0bfe935270f13e750a4af04cbb88a0eccf5
gharbiamine <aminegharbi@insat.u-carthage.tn>
2022-03-25 10:56:50 +0100

    stached local

--- src/pages/home-page/Homepage.js
+++ src/pages/home-page/Homepage.js
@@ -4,13 +4,7 @@ import styles from "./homepage.module.css";
 const Homepage = () => {
   return (
     <div className={styles.global}>
-      <h1>
-        Ea exercitation elit et non sit do non ad nisi do. Dolor id mollit duis
-        sunt proident voluptate anim esse quis dolore. Excepteur reprehenderit
-        aute nostrud eu id sint ullamco occaecat est cillum proident duis
-        nostrud dolore. Aute laboris id nostrud enim dolor cillum eiusmod ut id
-        occaecat qui tempor amet.
-      </h1>
+      <h1></h1>
     </div>
   );
 };
